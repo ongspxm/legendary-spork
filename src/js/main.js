1a7a576ade745aae97177aa94edcbdaca905e547
Metta Ong <ongspxm@gmail.com>
2018-07-31 18:53:18 +0800

    room listing for frontend completed
    Squashed commit of the following:
    
    commit 20175de6a2c1419e8d4baefa888c42d2596e7cef
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Tue Jul 31 18:47:29 2018 +0800
    
        converting into vue module for room
    
    commit f87c9591d629ce030e6e629a476c85c263ec4f77
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Mon Jul 30 23:49:45 2018 +0800
    
        basic framework for list
    
    commit 6112a33b89da19c66b8b2e853ae89b6eec739357
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Sat Jun 30 23:18:03 2018 +0800
    
        added source for frontend

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    rooms: [{
+      id: 1,
+      title: 'room 1',
+      duration: '3 months',
+      vacancy: 4,
+      gender: {
+        male: true,
+        female: false,
+      },
+      food: true,
+      imgs: [
+        'http://placekitten.com/300/200',
+        'http://placekitten.com/300/201',
+        'http://placekitten.com/300/202',
+        'http://placekitten.com/300/203',
+        'http://placekitten.com/300/204',
+        'http://placekitten.com/300/205',
+      ],
+    },{
+      id: 2,
+      title: 'room 2',
+      duration: '5 months',
+      gender: {
+        female: true,
+      },
+      imgs: [
+        'http://placekitten.com/300/204',
+        'http://placekitten.com/300/201',
+        'http://placekitten.com/300/202',
+        'http://placekitten.com/300/203',
+        'http://placekitten.com/300/204',
+        'http://placekitten.com/300/205',
+      ],
+    }]
+  },
+  components: {
+    v_room: window.vRoom,
+  }
+});
---
+++ src/js/v_room.js
@@ -0,0 +1,7 @@
+window.vRoom = {
+  props: [ 'room' ],
+  template: document.getElementById('v_room').innerHTML,
+  data: () => ({
+    imgIdx:1
+  }),
+};
