49f7aaa4b5c5568d603e4f9e18f05d1b964b9f84
Metta Ong <ongspxm@gmail.com>
2018-08-06 12:22:54 +0800

    frontend. reponsive search filter navbar
    commit 58a359e3a4a638ffde1de7bc02b21bfc6c0fc34c
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Mon Aug 6 12:14:39 2018 +0800
    
        frontend. navbar: search filter for desktop
    
    commit 98cf76df102dd7a88efed52b36f4e1b5b2464114
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Mon Aug 6 00:37:31 2018 +0800
    
        frontend. navbar: responsive mobile search filter
    
    commit fa36702155f4a91bc312ecf25bfc92b88f8d4a77
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Wed Aug 1 00:12:10 2018 +0800
    
        frontend: desktop view with navbar
    
    commit 22d78ae336d33cea499fa37e2930da73482382c0
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Sun Aug 5 19:58:55 2018 +0800
    
        frontend. added sample list of results from rooms

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 const app = new Vue({
   el: '#app',
   data: {
+    showFilter: false,
+    filter:{
+      gender:'',
+      meals:'',
+    },
     rooms: [{
       id: 1,
       title: 'room 1',
